3186c020f25738fb0dfc602f1e92a6c5ca36b19b
aravitej <aravitej@DIN73003066.corp.capgemini.com>
2018-11-26 09:48:37 +0530

    First commit

---
+++ GitExample2/src/main/java/com/cap/Example.java
@@ -0,0 +1,8 @@
+package com.cap;
+
+public class Example {
+	public static void main(String[] args) {
+		System.out.println("begining of the program");
+	}
+
+}
---
+++ MergeExample/src/main/java/Example/MergeP.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class MergeP {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world before any changes in another work space");
+	}
+
+}
